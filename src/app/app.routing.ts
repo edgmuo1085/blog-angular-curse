06f2725c317b91ff4eb51e764e5cdc1eeb5299ba
Developer_03 <desarrollo3@autodenar.com>
2020-07-08 17:36:34 -0500

    Sitio web con angular consumiendo de una apirest full con symfony

---
+++ src/app/app.routing.ts
@@ -0,0 +1,25 @@
+// Importes necesarios 
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Importar componentes
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ErrorComponent } from './components/error/error.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+
+// Definir Rutas
+const appRoutes: Routes = [
+	{path: '', component: HomeComponent},
+	{path: 'inicio', component: HomeComponent},
+	{path: 'login', component: LoginComponent},
+	{path: 'logout/:sure', component: LoginComponent},
+	{path: 'registro', component: RegisterComponent},
+	{path: 'ajustes', component: UserEditComponent},
+	{path: '**', component: ErrorComponent}
+];
+
+// Exportar rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
---
+++ src/app/models/video.ts
@@ -0,0 +1,10 @@
+export class Video {
+	constructor(
+		public id: number,
+		public title: string,
+		public description: string,
+		public url: string,
+		public status: string,
+		public user_id: string
+	){}
+}
